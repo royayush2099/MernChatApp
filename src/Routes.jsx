a629a3d4e70fba152979998439c6c2ec4926faed
Ayushroy <royayush2099@gmail.com>
2024-10-20 11:32:47 +0530

    creating ui for chat page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,12 @@
 import  { useContext } from 'react'
 import { UserContext } from './UserContext';
 import RegisterAndLogin from './RegisterAndLogin';
+import Chat from './Chat';
 
 const Routes = () => {
     const {username,id} = useContext(UserContext);
     if(username){
-      return 'Loggid In!'+username;
+      return <Chat/>
     }
 
 
