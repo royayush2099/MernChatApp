cc5d2736c42b91a33648d79ec6bc87d727a33a05
Ayushroy <royayush2099@gmail.com>
2024-10-27 13:35:07 +0530

    completed fetching messages from 1 user to another done with showing people who are online also added contact.jsx and fixed some vulnerabilities

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,170 +1,213 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import Avatar from './Avatar';
-import Logo from './Logo';
-import { UserContext } from './UserContext';
-import axios from 'axios';
+import {useContext, useEffect, useRef, useState} from "react";
+import Avatar from "./Avatar";
+import Logo from "./Logo";
+import {UserContext} from "./UserContext.jsx";
+import {uniqBy} from "lodash";
+import axios from "axios";
+import Contact from "./Contact";
 
-const Chat = () => {
-  // State to store WebSocket connection
-  const [ws, setWs] = useState(null);
-  // State to store online users
-  const [onlinePeople, setOnlinePeople] = useState({});
-  // State to track selected user for chat
-  const [selectedUserId, setSelectedUserId] = useState(null);
-  //state for messages
+export default function Chat() {
+  const [ws,setWs] = useState(null);
+  const [onlinePeople,setOnlinePeople] = useState({});
+  const [offlinePeople,setOfflinePeople] = useState({});
+  const [selectedUserId,setSelectedUserId] = useState(null);
   const [newMessageText,setNewMessageText] = useState('');
-  //state for storing messages
   const [messages,setMessages] = useState([]);
-  const { username, id } = useContext(UserContext);
+  const {username,id,setId,setUsername} = useContext(UserContext);
   const divUnderMessages = useRef();
-
   useEffect(() => {
-    // Establish WebSocket connection
+    connectToWs();
+  }, [selectedUserId]);
+  function connectToWs() {
     const ws = new WebSocket('ws://localhost:4000');
     setWs(ws);
-
-    // Add event listener for receiving messages
     ws.addEventListener('message', handleMessage);
-
-    // Cleanup function to close the WebSocket connection when component unmounts
-    return () => {
-      ws.close();
-    };
-  }, []);
-
-  // Function to update the online users' list
+    ws.addEventListener('close', () => {
+      setTimeout(() => {
+        console.log('Disconnected. Trying to reconnect.');
+        connectToWs();
+      }, 1000);
+    });
+  }
   function showOnlinePeople(peopleArray) {
     const people = {};
-    peopleArray.forEach(({ userId, username }) => {
+    peopleArray.forEach(({userId,username}) => {
       people[userId] = username;
     });
     setOnlinePeople(people);
   }
-
-  // Function to handle incoming WebSocket messages
   function handleMessage(ev) {
     const messageData = JSON.parse(ev.data);
-    console.log({ev})
-    // Check if the message contains online user info
+    console.log({ev,messageData});
     if ('online' in messageData) {
       showOnlinePeople(messageData.online);
-    }else if('text' in messageData){
-     setMessages(prev=>([...prev,{...messageData}]));
+    } else if ('text' in messageData) {
+      if (messageData.sender === selectedUserId) {
+        setMessages(prev => ([...prev, {...messageData}]));
+      }
     }
   }
-//function to send message
-  function sendMessage(ev){
-   
-ev.preventDefault(ev);
-ws.send(JSON.stringify({
- 
-    recepient: selectedUserId,
-    text:newMessageText,
+  function logout() {
+    axios.post('/logout').then(() => {
+      setWs(null);
+      setId(null);
+      setUsername(null);
+    });
   }
-));
-setNewMessageText('')
-setMessages(prev => ([...prev,{
-  text:newMessageText,
-   sender:id,
-   recepient:selectedUserId,
-   id:Date.now(),
-  
-  }]))
-  }
-  useEffect(()=>{
-    const div =  divUnderMessages.current;
-    if(div){
-      div.scrollIntoView({behavior:'smooth',block:'end'});//this useeffect is used to slide the mssg section down 
+  function sendMessage(ev, file = null) {
+    if (ev) ev.preventDefault();
+    ws.send(JSON.stringify({
+      recipient: selectedUserId,
+      text: newMessageText,
+      file,
+    }));
+    if (file) {
+      axios.get('/messages/'+selectedUserId).then(res => {
+        setMessages(res.data);
+      });
+    } else {
+      setNewMessageText('');
+      setMessages(prev => ([...prev,{
+        text: newMessageText,
+        sender: id,
+        recipient: selectedUserId,
+        _id: Date.now(),
+      }]));
     }
+  }
+  function sendFile(ev) {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = () => {
+      sendMessage(null, {
+        name: ev.target.files[0].name,
+        data: reader.result,
+      });
+    };
+  }
 
-  },[messages])
+  useEffect(() => {
+    const div = divUnderMessages.current;
+    if (div) {
+      div.scrollIntoView({behavior:'smooth', block:'end'});
+    }
+  }, [messages]);
 
-  useEffect(()=>{
-if(selectedUserId){
-  axios.get('/messages/'+selectedUserId)
-}
-  },[selectedUserId])
+  useEffect(() => {
+    axios.get('/people').then(res => {
+      const offlinePeopleArr = res.data
+        .filter(p => p._id !== id)
+        .filter(p => !Object.keys(onlinePeople).includes(p._id));
+      const offlinePeople = {};
+      offlinePeopleArr.forEach(p => {
+        offlinePeople[p._id] = p;
+      });
+      setOfflinePeople(offlinePeople);
+    });
+  }, [onlinePeople]);
 
+  useEffect(() => {
+    if (selectedUserId) {
+      axios.get('/messages/'+selectedUserId).then(res => {
+        setMessages(res.data);
+      });
+    }
+  }, [selectedUserId]);
 
-
-  // Exclude the current user from the list of online people
-  const onlinePeopleExclOurUser = { ...onlinePeople };
+  const onlinePeopleExclOurUser = {...onlinePeople};
   delete onlinePeopleExclOurUser[id];
 
-  return (
-    <div className='flex h-screen'>
-      {/* Sidebar for online users */}
-      <div className="bg-white w-1/3">
-        <Logo />
-        {/* List of online users */}
-        {Object.keys(onlinePeopleExclOurUser).map(userId => (
-          <div 
-            key={userId}
-            onClick={() => setSelectedUserId(userId)}
-            className={`border-b border-gray-100 flex items-center gap-2 cursor-pointer ${userId === selectedUserId ? 'bg-blue-200' : ''}`}
-          >
-            {/* Highlighted bar for selected user */}
-            {userId === selectedUserId && (
-              <div className='w-1 bg-blue-500 h-12 rounded-md'></div>
-            )}
-            <div className="flex gap-2 py-2 pl-2 items-center">
-              <Avatar username={onlinePeople[userId]} userId={userId} />
-              <span className='text-gray-800'>{onlinePeople[userId]}</span>
-            </div>
-          </div>
-        ))}
-      </div>
+  const messagesWithoutDupes = uniqBy(messages, '_id');
 
-      {/* Main chat area */}
-      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
-        <div className='flex-grow'>{!selectedUserId && (
-          <div className='flex  h-full items-center justify-center'> 
-          <div className='text-gray-400'>
-         &larr; Select a person from sidebar
-          </div>
-          </div>
-        )}
-        {!!selectedUserId && (
-          
- <div className='relative h-full '>
-     <div className='overflow-y-scroll absolute top-0 left-0 right-0 bottom-2'>{/**to pritn all the messages */}
-            {messages.map(message =>(
-              <div className={(message.sender === id ? 'text-right': 'text-left')}>
-  <div className={'text-left inline-block p-2 my-2 rounded-md text-sm '+(message.sender === id ? 'bg-blue-500 text-white': 'bg-white text-gray-500')}> 
-               sender:{message.sender}<br/>
-               my id:{id}<br/>
-               {message.text}</div> </div>
-            ))}
-            <div  ref={divUnderMessages}> </div>
-          </div> 
-           </div>
-        
-     
-        )}
+  return (
+    <div className="flex h-screen">
+      <div className="bg-white w-1/3 flex flex-col">
+        <div className="flex-grow">
+          <Logo />
+          {Object.keys(onlinePeopleExclOurUser).map(userId => (
+            <Contact
+              key={userId}
+              id={userId}
+              online={true}
+              username={onlinePeopleExclOurUser[userId]}
+              onClick={() => {setSelectedUserId(userId);console.log({userId})}}
+              selected={userId === selectedUserId} />
+          ))}
+          {Object.keys(offlinePeople).map(userId => (
+            <Contact
+              key={userId}
+              id={userId}
+              online={false}
+              username={offlinePeople[userId].username}
+              onClick={() => setSelectedUserId(userId)}
+              selected={userId === selectedUserId} />
+          ))}
         </div>
-{/*logic to not see the send message form if we haven't selected the user still  */}
-{!!selectedUserId && (
- <form className='flex gap-2' onSubmit={sendMessage}>
- {/* Input for typing new messages */}
- <input
-   type="text"
-   value={newMessageText}
-   onChange={ev => setNewMessageText(ev.target.value)}
-   placeholder="Type your message"
-   className="bg-white border p-2 flex-grow rounded-sm"
- />
- {/* Send button */}
- <button type='submit' className='bg-blue-500 p-2 text-white rounded-sm'>
-   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-   </svg>
- </button>
-</form>
-)}
- 
+        <div className="p-2 text-center flex items-center justify-center">
+          <span className="mr-2 text-sm text-gray-600 flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+              <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+            </svg>
+            {username}
+          </span>
+          <button
+            onClick={logout}
+            className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
+        </div>
+      </div>
+      <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="flex h-full flex-grow items-center justify-center">
+              <div className="text-gray-300">&larr; Select a person from the sidebar</div>
+            </div>
+          )}
+          {!!selectedUserId && (
+            <div className="relative h-full">
+              <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
+                {messagesWithoutDupes.map(message => (
+                  <div key={message._id} className={(message.sender === id ? 'text-right': 'text-left')}>
+                    <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " +(message.sender === id ? 'bg-blue-500 text-white':'bg-white text-gray-500')}>
+                      {message.text}
+                      {message.file && (
+                        <div className="">
+                          <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                              <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
+                            </svg>
+                            {message.file}
+                          </a>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+                <div ref={divUnderMessages}></div>
+              </div>
+            </div>
+          )}
+        </div>
+        {!!selectedUserId && (
+          <form className="flex gap-2" onSubmit={sendMessage}>
+            <input type="text"
+                   value={newMessageText}
+                   onChange={ev => setNewMessageText(ev.target.value)}
+                   placeholder="Type your message here"
+                   className="bg-white flex-grow border rounded-sm p-2"/>
+            <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
+              <input type="file" className="hidden" onChange={sendFile} />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
+              </svg>
+            </label>
+            <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
-};
-
-export default Chat;
+}
