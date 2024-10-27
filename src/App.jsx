1af576b1be3a29ff45d1021fd8ce8e9f928b9b55
Ayushroy <royayush2099@gmail.com>
2024-10-27 14:01:39 +0530

    changin url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Routes from "./Routes";
 
 
 function App() {
-axios.defaults.baseURL= 'http://localhost:4000';
+axios.defaults.baseURL= 'https://chxt-backend.onrender.com';
 axios.defaults.withCredentials = true;
 
 
